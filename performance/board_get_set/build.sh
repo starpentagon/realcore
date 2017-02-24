db704b01b5c907fb52463cc4aa938078646d7387
starpentagon <starpentagon@gmail.com>
2017-02-24 13:48:06 +0900

    Modify build script.

--- performance/board_get_set/build.sh
+++ performance/board_get_set/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-compiler=clang++
+compiler=g++
 
 if [ ! -d build ]; then
 	mkdir build
@@ -7,7 +7,7 @@ fi
 
 pushd build
 
-cmake -DCMAKE_CXX_COMPILER=${compiler} -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_CXX_COMPILER=${compiler} ..
 make -j ${MAKE_JOB_NUMBER}
 
 popd
