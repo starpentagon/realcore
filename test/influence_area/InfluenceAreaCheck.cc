c61c11bb656d95b356c082aa8d3357791dbdc82e
starpentagon <starpentagon@gmail.com>
2017-04-30 23:30:42 +0900

    Implement InfluenceArea calculation in IsForbiddenMove

--- test/influence_area/InfluenceAreaCheck.cc
+++ test/influence_area/InfluenceAreaCheck.cc
@@ -4,7 +4,6 @@
 #include <boost/program_options.hpp>
 
 #include "InfluenceAreaCheck.h"
-#include "CSVReader.h"
 #include "Board.h"
 #include "HashTable.h"
 
@@ -19,6 +18,8 @@ int main(int argc, char* argv[])
   
   option.add_options()
     ("db", value<string>(), "棋譜データベース(csv)")
+    ("line", "直線近傍によるパターン判定テスト(長連/四々/見かけの三々)")
+    ("dbl-three", "三々判定テスト")
     ("help,h", "ヘルプを表示");
   
   variables_map arg_map;
@@ -41,17 +42,33 @@ int main(int argc, char* argv[])
   map<string, StringVector> diagram_db;
   ReadCSV(diagram_db_file, &diagram_db);
 
-  const auto board_string_list = diagram_db["game_record"];
-  const auto record_count = board_string_list.size();
+  const auto record_string_list = diagram_db["game_record"];
+  const auto record_count = record_string_list.size();
   cerr << "Game count: " << record_count << endl;
 
   // LineNeighborhood::ForbiddenCheckの影響領域チェック
-  cerr << "LineNeighborhood::ForbiddenCheck" << endl;
+  if(arg_map.count("line")){
+    cerr << "LineNeighborhood::ForbiddenCheck" << endl;
+    LineNeighborhoodForbiddenTest(record_string_list);
+  }
+  
+  // 三々のチェック
+  if(arg_map.count("dbl-three")){
+    cerr << "BitBoard::IsForbiddenMove" << endl;
+    DoubleThreeTest(record_string_list);
+  }
+
+  return 0;
+}
+
+void LineNeighborhoodForbiddenTest(const realcore::StringVector &record_string_list)
+{
   map<string, SearchCounter> line_neighborhood_forbidden_check;
   set<HashValue> checked_board;
+  const auto record_count = record_string_list.size();
 
   for(size_t i=0; i<record_count; ++i){
-    const auto& board_string = board_string_list[i];
+    const auto& board_string = record_string_list[i];
     MoveList board_move_sequence(board_string);
     MoveList board_move;
 
@@ -86,8 +103,6 @@ int main(int argc, char* argv[])
   cerr << "  Exact: " << line_neighborhood_forbidden_check[kCheckExactNonForbidden] << endl;
   cerr << "  Calc: " << line_neighborhood_forbidden_check[kCheckCalcNonForbidden] << endl;
   cerr << "  Calc/Exact: " << 1.0 * line_neighborhood_forbidden_check[kCheckCalcNonForbidden] / line_neighborhood_forbidden_check[kCheckExactNonForbidden] << endl;
-
-  return 0;
 }
 
 void LineNeighborhoodForbiddenCheck(const realcore::MoveList &board_sequence, std::map<std::string, realcore::SearchCounter> *check_result)
@@ -115,8 +130,10 @@ void LineNeighborhoodForbiddenCheck(const realcore::MoveList &board_sequence, st
     line_neighbor.SetCenterState<kBlackStone>();
 
     vector<BoardPosition> next_open_four_list;
-    MoveBitSet influence_area;
-    const ForbiddenCheckState forbidden_state = line_neighbor.ForbiddenCheck(&next_open_four_list, &influence_area);
+    MoveBitSet downward_influence_area, upward_influence_area;
+    const ForbiddenCheckState forbidden_state = line_neighbor.ForbiddenCheck(&next_open_four_list, &downward_influence_area, &upward_influence_area);
+
+    const MoveBitSet &influence_area = (forbidden_state == kForbiddenMove || forbidden_state == kPossibleForbiddenMove) ? downward_influence_area : upward_influence_area;
 
     // 近傍空点に石を追加して結果が変わる領域を求める
     MoveList neighbor_open_move;
@@ -191,6 +208,135 @@ void LineNeighborhoodForbiddenCheck(const realcore::MoveList &board_sequence, st
     }else{
       (*check_result)[kCheckExactNonForbidden] += exact_influence_area.count();
       (*check_result)[kCheckCalcNonForbidden] += influence_area.count();
+    }
+  }
+}
+
+void DoubleThreeTest(const realcore::StringVector &record_string_list)
+{
+  map<string, SearchCounter> double_three_forbidden_check;
+  set<HashValue> checked_board;
+  const auto record_count = record_string_list.size();
+
+  for(size_t i=0; i<record_count; ++i){
+    const auto& board_string = record_string_list[i];
+    MoveList board_move_sequence(board_string);
+    MoveList board_move;
+
+    for(const auto move : board_move_sequence){
+      if(board_move.IsBlackTurn()){
+        const auto hash_value = CalcHashValue(board_move);
+
+        if(checked_board.find(hash_value) == checked_board.end()){
+          DoubleThreeCheck(board_move, &double_three_forbidden_check);
+          checked_board.insert(hash_value);
+        }
+      }
+
+      board_move += move;
+    }
+
+    if(i % 1000 == 0){
+      cerr << i << endl;
+    }
+  }
+
+  // BitBoard::IsForbiddenMoveの影響領域チェック結果
+  cerr << "DoubleThree:" << endl;
+  cerr << "  Exact: " << double_three_forbidden_check[kExactCheckDoubleThree] << endl;
+  cerr << "  Calc: " << double_three_forbidden_check[kCalcCheckDoubleThree] << endl;
+  cerr << "  Calc/Exact: " << 1.0 * double_three_forbidden_check[kCalcCheckDoubleThree] / double_three_forbidden_check[kExactCheckDoubleThree] << endl;
+  cerr << "NonDoubleThree:" << endl;
+  cerr << "  Exact: " << double_three_forbidden_check[kExactCheckNonDoubleThree] << endl;
+  cerr << "  Calc: " << double_three_forbidden_check[kCalcCheckNonDoubleThree] << endl;
+  cerr << "  Calc/Exact: " << 1.0 * double_three_forbidden_check[kCalcCheckNonDoubleThree] / double_three_forbidden_check[kExactCheckNonDoubleThree] << endl;
+}
+
+void DoubleThreeCheck(const realcore::MoveList &board_sequence, std::map<std::string, realcore::SearchCounter> *check_result)
+{
+  const bool is_black_turn = board_sequence.IsBlackTurn();
+
+  if(!is_black_turn){
+    return;
+  }
+
+  MoveList candidate_move;
+  board_sequence.GetOpenMove(&candidate_move);
+
+  BitBoard bit_board(board_sequence);
+  BoardOpenState board_open_state;
+  bit_board.GetBoardOpenState(kUpdateForbiddenCheck, &board_open_state);
+
+  MoveBitSet double_semi_three;
+  bit_board.EnumerateDoubleSemiThreeMoves<kBlackTurn>(board_open_state, &double_semi_three);
+
+  MoveList double_semi_three_move_list;
+  GetMoveList(double_semi_three, &double_semi_three_move_list);
+
+  for(const auto move : double_semi_three_move_list){
+    MoveBitSet downward_influence_area, upward_influence_area;
+    const bool is_forbidden = bit_board.IsForbiddenMove<kBlackTurn>(move, &downward_influence_area, &upward_influence_area);
+    const MoveBitSet &influence_area = is_forbidden ? downward_influence_area : upward_influence_area;
+
+    MoveBitSet exact_influence_area;
+
+    if(is_forbidden){
+      exact_influence_area.set(move);
+    }
+    
+    for(const auto check_move : candidate_move){
+      if(check_move == move){
+        continue;
+      }
+      {
+      bit_board.SetState<kBlackStone>(check_move);
+
+      const bool check_forbidden = bit_board.IsForbiddenMove<kBlackTurn>(move);
+      
+      if(check_forbidden != is_forbidden){
+        exact_influence_area.set(check_move);
+      }
+      
+      bit_board.SetState<kOpenPosition>(check_move);
+      }
+      {
+      bit_board.SetState<kWhiteStone>(check_move);
+
+      const bool check_forbidden = bit_board.IsForbiddenMove<kBlackTurn>(move);
+      
+      if(check_forbidden != is_forbidden){
+        exact_influence_area.set(check_move);
+      }
+
+      bit_board.SetState<kOpenPosition>(check_move);
+      }
+    }
+
+    // 算出した影響領域が正確な影響領域を包含していることをチェック
+    const bool is_include = ((influence_area | exact_influence_area) == influence_area);
+
+    if(!is_include){
+      MoveList influence_move, exact_influence_move;
+      GetMoveList(influence_area, &influence_move);
+      GetMoveList(exact_influence_area, &exact_influence_move);
+      cout << "[Not Include]board: " << board_sequence.str() << " , Move: " << MoveString(move) << ", ";
+      cout << "exact: " << exact_influence_move.str() << " , calc: " << influence_move.str() << endl;
+    }
+
+    const auto exact_label = is_forbidden ? kExactCheckDoubleThree : kExactCheckNonDoubleThree;
+    const auto calc_label = is_forbidden ? kCalcCheckDoubleThree : kCalcCheckNonDoubleThree;
+
+    (*check_result)[exact_label] += exact_influence_area.count();
+    (*check_result)[calc_label] += influence_area.count();
+
+    // 差分領域のチェック
+    const auto diff_area = influence_area ^ exact_influence_area;
+
+    if(!is_forbidden && diff_area.count() >= 7){
+      MoveList diff_move;
+      GetMoveList(diff_area, &diff_move);
+      cout << "[Diff]board: " << board_sequence.str() << " , Move: " << MoveString(move) << ", ";
+      cout << "diff: " << diff_move.str() << endl;
     }
   }
 }
--- test/influence_area/InfluenceAreaCheck.h
+++ test/influence_area/InfluenceAreaCheck.h
@@ -7,6 +7,7 @@
 #include "MoveList.h"
 #include "BitBoard.h"
 #include "SearchManager.h"
+#include "CSVReader.h"
 
 static const std::string kCheckExactForbidden = "ExactForbidden";                   // 戻り値がkForbiddenMove時の正確な影響領域
 static const std::string kCheckExactPossibleForbidden = "ExactPossibleForbidden";   // 戻り値がkPossibleForbiddenMove時の正確な影響領域
@@ -17,5 +18,14 @@ static const std::string kCheckCalcPossibleForbidden = "CalcPossibleForbidden"; 
 static const std::string kCheckCalcNonForbidden = "CalcNonForbidden";             // 戻り値がkNonForbiddenMove時の算出した影響領域
 
 void LineNeighborhoodForbiddenCheck(const realcore::MoveList &board_sequence, std::map<std::string, realcore::SearchCounter> *check_result);
+void LineNeighborhoodForbiddenTest(const realcore::StringVector &record_string_list);
+
+static const std::string kExactCheckDoubleThree = "ExactCheckDoubleThree";
+static const std::string kExactCheckNonDoubleThree = "ExactCheckNonDoubleThree";
+static const std::string kCalcCheckDoubleThree = "CalcCheckDoubleThree";
+static const std::string kCalcCheckNonDoubleThree = "CalcCheckNonDoubleThree";
+
+void DoubleThreeCheck(const realcore::MoveList &board_sequence, std::map<std::string, realcore::SearchCounter> *check_result);
+void DoubleThreeTest(const realcore::StringVector &record_string_list);
 
 #endif
