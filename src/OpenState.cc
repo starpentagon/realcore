410eb5cb27943dd94fb647948a50a7ce0e799a2c
starpentagon <starpentagon@gmail.com>
2017-11-06 21:26:49 +0900

    Add GetStonePosition in OpenState.

--- src/OpenState.cc
+++ src/OpenState.cc
@@ -57,6 +57,43 @@ void OpenState::GetSemiThreePosition(array<BoardPosition, 2> * const semi_three_
   }
 }
 
+void OpenState::GetStonePosition(std::vector<BoardPosition> * const stone_position_list) const
+{
+  assert(pattern_ == kNextFourBlack || pattern_ == kNextFourWhite || 
+    pattern_ == kNextPointOfSwordBlack || pattern_ == kNextPointOfSwordWhite);
+  assert(stone_position_list != nullptr);
+  
+  if(pattern_ == kNextFourBlack || pattern_ == kNextFourWhite)
+  {
+    // 四ノビ点([B302][W3O2])
+    const auto open_index_1 = GetLessIndexOfTwo(pattern_search_index_);
+    const auto open_index_2 = GetGreaterIndexOfTwo(pattern_search_index_);
+
+    const auto open_position_1 = GetOpenBoardPosition(pattern_position_, open_index_1);
+    const auto open_position_2 = GetOpenBoardPosition(pattern_position_, open_index_2);
+
+    for(BoardPosition board_position=pattern_position_; board_position<pattern_position_+5; board_position++){
+      if(board_position == open_position_1 || board_position == open_position_2){
+        continue;
+      }
+
+      stone_position_list->emplace_back(board_position);
+    }
+  }else{
+    // 剣先点([B203][W2O3])
+    array<BoardPosition, 2> open_position_list;
+    GetFourPosition(&open_position_list);
+
+    for(BoardPosition board_position=pattern_position_; board_position<pattern_position_+5; board_position++){
+      if(board_position == open_position_ || board_position == open_position_list[0] || board_position == open_position_list[1]){
+        continue;
+      }
+
+      stone_position_list->emplace_back(board_position);
+    }
+  }
+}
+
 bool IsEqual(const OpenState &open_state_1, const OpenState &open_state_2)
 {
   if(open_state_1.pattern_ != open_state_2.pattern_){
